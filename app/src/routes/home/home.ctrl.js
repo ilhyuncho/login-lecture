d11ddf8307ac5ff69a32a218dfb46bc6658fc383
choihvit <85654045+choihvit@users.noreply.github.com>
2022-05-26 12:21:53 +0900

    백앤드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,46 @@
 "use strict";
 
 
-const hello = (req, res) => {
-    res.render("home/index");
+const output = {
+
+    hello : (req, res) => {
+        res.render("home/index");
+    },
+    
+    login : (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["cihg1", "나개발", "김팀장"],
+    psword: ["1234", "1233", "1234454"],
 };
+
+
+const process = {
+    login : (req, res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if( users.id.includes(id))
+        {
+            const index = users.id.indexOf(id);
+            if( users.psword[index] === psword )
+            {
+                return res.json({
+                    success: true,
+                });
+            } 
+        }
+        return res.json({
+            success: false,
+            msg:"로그인에 실패 하셨습니다.",
+        });
+    },
+};
+
 
 module.exports = {
-    hello, login,
-}
+    output, process,
+};
