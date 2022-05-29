c1da01fe089a801ce22bfdd8c7dfd9aee7d705da
choihvit <85654045+choihvit@users.noreply.github.com>
2022-05-29 13:57:37 +0900

    최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -26,23 +26,20 @@ const process = {
     login : async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if(response.err)
-        {
-            logger.error(
-                `POST /login 200 Response: "success: ${response.success}, ${response.err}"`  
-              );
-        }
-        else
-        {
-            logger.info(
-                `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`  
-            );
-        }
-        return res.json(response);
+        const url ={
+            method: "POST",
+            path:"/login",
+            status: response.err ? 400 : 200,
+        };
+        
+        log(response, url );
+        // 클라이언트에게 상태값 전달시..res.status
+        return res.status(url.status).json(response);
     },
     register : async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
+
         if(response.err)
         {
             logger.error(
@@ -52,9 +49,10 @@ const process = {
         else
         {
             logger.info(
-                `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`  
+                `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg || "" }"`  
             );
         }
+
         return res.json(response);
     },
 
@@ -65,3 +63,18 @@ const process = {
 module.exports = {
     output, process,
 };
+
+const log = (response, url) =>{
+    if(response.err)
+    {
+        logger.error(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`  
+          );
+    }
+    else
+    {
+        logger.info(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.msg || ""}`  
+        );
+    }
+}
